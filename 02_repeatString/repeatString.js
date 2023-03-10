f2a6bbdda8ff9a9152c83a8a9d7b3271e81d4f00
Jonathan Yanez <jonathanyanez02@gmail.com>
2023-03-09 22:39:27 -0500

    Passed 3 tests on first 3 files

--- 02_repeatString/repeatString.js
+++ 02_repeatString/repeatString.js
@@ -1,6 +1,12 @@
-const repeatString = function() {
-
-};
+const repeatString = function(word, num) {
+    let string = "";
+    for(let i=0;i<num;i++){
+    string += word;
+    }
+    return string;
+    };
+    
+    repeatString("st",4);
 
 // Do not edit below this line
 module.exports = repeatString;
--- 03_reverseString/reverseString.js
+++ 03_reverseString/reverseString.js
@@ -1,6 +1,16 @@
-const reverseString = function() {
+const reverseString = function (x) {
+
+    let word = "";
+
+    for (i = x.length - 1; i >= 0; i--) {
+        word += x[i];
+
+
+    }
+    return word;
 
 };
 
+reverseString("hello");
 // Do not edit below this line
 module.exports = reverseString;
