015432aa18c603e3db99d2d93b928cd9e2835730
Jonathan Yanez <jonathanyanez02@gmail.com>
2023-03-13 23:18:44 -0400

    Finished tests 04,04,06,07

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,6 +1,28 @@
-const sumAll = function() {
 
-};
+
+function sumAll(x, y) {
+    let sum = 0;
+  
+    try {
+      if (x < 0 || y < 0) {
+        throw Error('ERROR');
+      }
+  
+      if (x >= y) {
+        for (y; y <= x; y++) {
+          sum += y;
+        }
+      } else {
+        for (x; x <= y; x++) {
+          sum += x;
+        }
+      }
+    } catch (error) {
+      console.error(error.message);
+    }
+  
+    return sum;
+  }
 
 // Do not edit below this line
 module.exports = sumAll;
